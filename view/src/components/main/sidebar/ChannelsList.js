42c0469006ef0678148b8c30ee66303a47bbc667
Cloud9NB <blackseeds10@hotmail.com>
2022-08-19 11:58:50 -0700

    creaters channels list and css

---
+++ view/src/components/main/sidebar/ChannelsList.js
@@ -0,0 +1,10 @@
+import '../../../css/sideBar/ChannelsList.css';
+
+const ChannelsList = () => {
+  return (
+    <h4>
+      <span className='sidebar__channelList__hash'>#</span>Youtube
+    </h4>
+  );
+};
+export default ChannelsList;
