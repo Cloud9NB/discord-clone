b59e52078ae09424efe39fbfcef37e6a9ed0e25b
Cloud9NB <blackseeds10@hotmail.com>
2022-08-19 11:56:38 -0700

    creates sidebar with channels

--- view/src/components/main/sidebar/SideBar.js
+++ view/src/components/main/sidebar/SideBar.js
@@ -1,13 +1,22 @@
 import '../../../css/sideBar/Sidebar.css';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Channels from './Channels';
+import Voice from './Voice';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className='sidebar__top'>
-        <h3>March LHL Cohort</h3>
+        <h3>March 2022 Cohort</h3>
         <ExpandMoreIcon />
       </div>
+
+      <Channels />
+      <Channels />
+      <Channels />
+      <Channels />
+
+      <Voice />
     </div>
   );
 };
