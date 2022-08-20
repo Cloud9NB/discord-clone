21d57161c78ed20b29bd0b957ea8a68abb589199
Cloud9NB <blackseeds10@hotmail.com>
2022-08-19 20:00:56 -0700

    fixes css

--- view/src/components/main/sidebar/ChannelsList.js
+++ view/src/components/main/sidebar/ChannelsList.js
@@ -2,9 +2,13 @@ import '../../../css/sideBar/ChannelsList.css';
 
 const ChannelsList = () => {
   return (
-    <h4>
-      <span className='sidebar__channelList__hash'>#</span>Youtube
-    </h4>
+    <div className='sidebar__channelsList'>
+      <div className='sidebar__channels'>
+        <h4>
+          <span className='sidebar__channelList__hash'>#</span>Youtube
+        </h4>
+      </div>
+    </div>
   );
 };
 export default ChannelsList;
