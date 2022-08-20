daba40466804dcd4823375431a5fbaf54b2cb7f8
Cloud9NB <blackseeds10@hotmail.com>
2022-08-20 11:38:25 -0700

    creates an conditional if user is logged in or not to create login/signup components

--- view/src/components/App.js
+++ view/src/components/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
 import '../css/App.css';
 import Chat from './main/chat/Chat';
 import Sidebar from './main/sidebar/Sidebar';
+import { selectUser } from '../features/userSlice';
+import { useSelector } from 'react-redux';
 
 const App = () => {
+  const user = useSelector(selectUser);
+
   return (
     <div className='App'>
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <h1>Please Login</h1>
+      )}
     </div>
   );
 };
