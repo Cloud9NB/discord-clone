e79dc3ab9211f43b031b2ad01e06babcff4df9a5
Cloud9NB <blackseeds10@hotmail.com>
2022-08-20 23:46:43 -0700

    make the channel list name dynamic

--- view/src/components/main/chat/ChatHeader.js
+++ view/src/components/main/chat/ChatHeader.js
@@ -1,13 +1,17 @@
 import '../../../css/chat/ChatHeader.css';
 import ChatHeaderRight from './ChatHeaderRight';
+import { useSelector } from 'react-redux';
+import { selectChannelListName } from '../../../features/appSlice';
 
 const ChatHeader = () => {
+  const channelListName = useSelector(selectChannelListName);
+
   return (
     <div className='chatHeader'>
       <div className='chatHeader__left'>
         <h3>
           <span className='chatHeader__hash'>#</span>
-          Youtube
+          {channelListName}
         </h3>
       </div>
 
