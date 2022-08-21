b72792933cfa2b977f060967c97eb7e90ab4c1c9
Cloud9NB <blackseeds10@hotmail.com>
2022-08-20 17:30:27 -0700

    creates a working login button that contains users info and changes components

--- view/src/components/App.js
+++ view/src/components/App.js
@@ -1,12 +1,31 @@
 import '../css/App.css';
 import Chat from './main/chat/Chat';
 import Sidebar from './main/sidebar/Sidebar';
-import { selectUser } from '../features/userSlice';
-import { useSelector } from 'react-redux';
+import { login, logout, selectUser } from '../features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import Login from './Login';
-
+import { auth } from '../firebase/firebase';
+import { useEffect } from 'react';
 const App = () => {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
   return (
     <div className='App'>
