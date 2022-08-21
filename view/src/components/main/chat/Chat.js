2f306dd78f42bc66cc5e2ff1c012d8954ddc34a0
Cloud9NB <blackseeds10@hotmail.com>
2022-08-20 23:54:10 -0700

    creates a functioning form and displays the chat message dynamic

--- view/src/components/main/chat/Chat.js
+++ view/src/components/main/chat/Chat.js
@@ -1,20 +1,51 @@
+import { useEffect, useState } from 'react';
 import '../../../css/chat/Chat.css';
 import ChatHeader from './ChatHeader';
 import ChatInput from './ChatInput';
 import ChatMessages from './ChatMessages';
+import db from '../../../firebase/firebase';
+import { useSelector } from 'react-redux';
+import { selectChannelListId } from '../../../features/appSlice';
 
 const Chat = () => {
+  const [state, setState] = useState({
+    messages: [],
+    input: '',
+  });
+
+  const channelListId = useSelector(selectChannelListId);
+
+  const messageList = state.messages.map(message => (
+    <ChatMessages
+      key={message.timestamp.seconds}
+      message={message.messages}
+      timestamp={message.timestamp}
+      user={message.user}
+    />
+  ));
+
+  useEffect(() => {
+    if (channelListId) {
+      db.collection('channels')
+        .doc(channelListId)
+        .collection('messages')
+        .orderBy('timestamp', 'desc')
+        .onSnapshot(snapshot =>
+          setState(prev => ({
+            ...prev,
+            messages: snapshot.docs.map(doc => doc.data()),
+          }))
+        );
+    }
+  }, [channelListId]);
+
   return (
     <div className='chat'>
       <ChatHeader />
 
-      <div className='chat__messages'>
-        <ChatMessages />
-        <ChatMessages />
-        <ChatMessages />
-      </div>
+      <div className='chat__messages'>{messageList}</div>
 
-      <ChatInput />
+      <ChatInput setState={setState} input={state.input} />
     </div>
   );
 };
