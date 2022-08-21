6d72e7b75b50ac9e737c5378846a6dcc9f1e3bbb
Cloud9NB <blackseeds10@hotmail.com>
2022-08-20 18:07:26 -0700

    makes profile component with dynanmic information

--- view/src/components/main/sidebar/Profile.js
+++ view/src/components/main/sidebar/Profile.js
@@ -3,14 +3,19 @@ import { Avatar } from '@mui/material';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../../features/userSlice';
 
 const Profile = () => {
+  const { photo, displayName, uid } = useSelector(selectUser);
+  const NumberOnlyUid = uid.replace(/\D/g, '');
+
   return (
     <div className='sidebar__profile'>
-      <Avatar />
+      <Avatar src={photo} />
       <div className='sidebar__profileInfo'>
-        <h3>@Cloud9NB</h3>
-        <p>#4223</p>
+        <h3>@{displayName}</h3>
+        <p>#{NumberOnlyUid}</p>
       </div>
 
       <div className='sidebar__profileIcons'>
@@ -21,4 +26,5 @@ const Profile = () => {
     </div>
   );
 };
+
 export default Profile;
